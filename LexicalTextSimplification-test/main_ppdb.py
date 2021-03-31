6f9a53d296b8aa78af03410ae0c761c60e1d98d6
Raghav Roy <raghavroy145@gmail.com>
2021-03-31 17:13:33 +0530

    added repository-shakurova for test

---
+++ LexicalTextSimplification-test/main_ppdb.py
@@ -0,0 +1,112 @@
+# import ppdb
+# import cPickle as pickle
+import os
+import pickle
+# import ujson
+import editdistance
+import nltk
+
+def remove_comma_and_article(expression):
+    """
+    Filter an expression by removing any leading articles and/or commas.
+
+    :param expression: a list/tuple of strings
+    :return: a list of strings
+    """
+
+    articles = {'a', 'the'}
+    if len(expression) == 1:
+        return expression
+
+    while expression[0] in articles or expression[0] == ',':
+        expression = expression[1:]
+        if len(expression) == 0:
+            return expression
+
+    if expression[-1] == ',':
+        expression = expression[:-1]
+
+    return expression
+
+
+def is_trivial(exp1, exp2, relation):
+    """
+    Return True if:
+        The words have the same stem or are only 1 symbol apart
+
+    :param exp1: tuple/list of strings, expression1
+    :param exp2: tuple/list of strings, expression2
+    :return: boolean
+    """
+
+    endings = ['ing', 'ed', 's', 'ic']
+    ps = nltk.PorterStemmer()
+
+    # Contains many false positives
+    if relation == 'ReverseEntailment':  # don't include
+        return True
+
+    if (editdistance.eval(exp1[0], exp2[0]) <= 1):
+        return True
+
+
+    if ps.stem(exp1[0]) == ps.stem(exp2[0]):
+        return True
+
+    return False
+
+
+def load_ppdb(path='.data/ppdb-2.0-s-lexical', load_pickle = True):
+    """
+    Load and filter the paraphrases, saving the filtered results for faster loading later
+    :param path: to the ppdb database to load
+    :param load_pickle: whether to load from pickle if such file exists
+    :return: boolean
+    """
+
+    PICKLE_PATH = path + '.pkl'
+    if load_pickle and os.path.isfile(PICKLE_PATH):
+        return pickle.load(open(PICKLE_PATH, 'rb'))
+    else:
+        ppdb_rules = {}
+        with open(path, 'r') as f:
+            for line in f:
+                # Discard lines with unrecoverable encoding errors
+                if '\\ x' in line or 'xc3' in line:
+                    continue
+                fields = line.split('|||')
+                lhs = fields[1].strip().split()
+                rhs = fields[2].strip().split()
+                relation = fields[-1].strip()
+
+
+                lhs = remove_comma_and_article(lhs)
+                rhs = remove_comma_and_article(rhs)
+
+                # We only care about 1-word replacements
+                if len(lhs) != 1 or len(rhs) != 1:
+                    continue
+
+                # filter out trivial variations
+                if is_trivial(lhs, rhs, relation):
+                    # print(lhs, rhs, relation)
+                    continue
+
+                # Add rhs to the transformation dictionary
+                if lhs[0] == 'and':
+                    print('...')
+                if lhs[0] not in ppdb_rules:
+                    ppdb_rules[lhs[0]] = set()
+                ppdb_rules[lhs[0]].add(rhs[0])
+        print("Nr of rules: ", len(ppdb_rules))
+        pickle.dump(ppdb_rules, open(PICKLE_PATH, 'wb'))
+        return ppdb_rules
+
+
+if __name__ == '__main__':
+    rules = load_ppdb(path='./data/ppdb-2.0-xxl-lexical', load_pickle=False)
+    input = 'These theorists were sometimes only loosely affiliated, and some authors point out that the "Frankfurt circle" was neither a philosophical school nor a political group.'
+
+    for word in input.split():
+        if word in rules:
+            print(word + " -> "  + str(rules[word]))
---
+++ LexicalTextSimplification-test/script.py
@@ -0,0 +1,46 @@
+import re
+import pandas as pd
+import ujson
+import _pickle as pickle
+from nltk import sent_tokenize, word_tokenize, pos_tag
+import pandas as pd
+import gensim
+
+from nltk.corpus import brown
+from nltk.probability import *
+from nltk.corpus import wordnet
+from nltk import sent_tokenize, word_tokenize, pos_tag
+
+import logging
+
+import text_simplification
+from conjugation import convert
+
+
+def concat(row):
+    return str(row['w1']) + ' ' + str(row['w2'])
+
+
+def generate_freq_dict():
+    """ Create frequency dictionary based on BROWN corpora. """
+    freq_dict = FreqDist()
+    for sentence in brown.sents():
+        for word in sentence:
+            freq_dict[word] += 1
+    return freq_dict
+
+
+if __name__ == '__main__':
+
+    simplifier = text_simplification.Simplifier()
+    with open('./evaluation/test.en') as f:
+        with open('./evaluation/test0.lsen', 'w') as s0, open('./evaluation/test1.lsen', 'w') as s1, open('./evaluation/test2.lsen', 'w') as s2:
+            for input in f:
+                simplified0, simplified1, simplified2 = simplifier.simplify(input)
+                s0.writelines(simplified0 + "\n")
+                # print("writing output1")
+                s1.writelines(simplified1 + "\n")
+                # print("writing output2")
+                s2.writelines(simplified2 + "\n")
+                # print("writing output1")
+                
